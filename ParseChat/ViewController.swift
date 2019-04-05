aabb2d810b70c09c9b89c7900cc53b0ac913ba03
Steven Tran <steventraaan@gmail.com>
2019-04-04 18:08:05 -0700

    Initial Commit

---
+++ ParseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Steven Tran on 4/4/19.
+//  Copyright © 2019 Steven Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
